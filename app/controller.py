583f17a2ae61586f2007cb56b3f41ef26065350b
varzeny <varzeny@gmail.com>
2024-07-13 20:59:31 +0900

    로그인 구현 시작. 토큰기능 구현할할 것

--- app/controller.py
+++ app/controller.py
@@ -4,23 +4,31 @@
 from fastapi import Depends
 from fastapi.routing import APIRouter
 from fastapi.requests import Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
 
 ## module
 from app.core.database_manager import DatabaseManager as DB
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text
+from app.schema.endpoint_schema import LoginSchema, SignSchema
 
 ## definition
 router = APIRouter()
-
+template = Jinja2Templates(directory="app/template")
 
 ## endpoint
 
 
 ### test
 @router.get("/test")
-async def get_test( ss:AsyncSession=Depends( DB.get_ss ) ):
+
+
+
+### sign up
+
+@router.post("/sign")
+async def post_sign( ss:AsyncSession=Depends( DB.get_ss ) ):
     name="kim"
     password="123"
     query = """
@@ -37,12 +45,47 @@ async def get_test( ss:AsyncSession=Depends( DB.get_ss ) ):
 
 
 
-@router.get("/")
-async def get_root():
-    return JSONResponse( content={"data":"hello!"}, status_code=200 )
+### widget
+@router.get("/widget-account")
+async def get_accountbar(req:Request):
+    return template.TemplateResponse( "widget_account.html", {"request":req} )
 
 
-### login
+### page
+@router.get("/page-login")
+async def get_page_login(req:Request):
+    return template.TemplateResponse( "page_login.html", {"request":req} )
+
+
+### general
+#### login
 @router.post("/login")
-async def post_login():
-    print()
+async def post_login( data:LoginSchema, ss:AsyncSession=Depends( DB.get_ss ) ):
+    print(type(data), data)
+    
+    query = """
+    SELECT * FROM account WHERE name_=:name AND password_=:password;
+    """
+    result = await ss.execute(
+        text(query),
+        params={ "name":data.name, "password":data.password }
+    )
+    results = result.all()
+
+    if results:
+        print(type(results[0]), results[0])
+        resp = JSONResponse( content={}, status_code=200 )
+        # 토큰 발행 구현할 것, 토큰 확인도 구현할것
+        new_token = 
+
+        # httponly 쿠키 넣어주기
+        resp.set_cookie(
+            key="login_token",
+            value=new_token,
+            httponly=True,
+            max_age=3600,
+            path="/"
+        )
+        return resp
+    else:
+        return JSONResponse( content={}, status_code=404 )
---
+++ app/schema/endpoint_schema.py
@@ -0,0 +1,19 @@
+# endpoint_schema.py
+
+## lib
+from pydantic import BaseModel, Field
+
+
+### signup
+
+
+### login
+class LoginSchema(BaseModel):
+    name:str = Field( default=..., min_length=3, max_length=45 )
+    password:str = Field( default=..., min_length=3, max_length=45 )
+
+
+class SignSchema(BaseModel):
+    name:str = Field( default=..., min_length=3, max_length=45 )
+    password:str = Field( default=..., min_length=3, max_length=45 )
+    email:str = Field( default=..., min_length=3, max_length=45 )
